e67d4ba375dab03dbee7e5609c4b1beba91f54bc
Brian Nguyen <thuann89@gmail.com>
2017-08-29 17:09:12 +0700

    Add test for mongodb

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-const conn = process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp';
+const conn = process.env.MONGODB_URI;
 mongoose.connect(conn);
 
 module.exports = { mongoose };
